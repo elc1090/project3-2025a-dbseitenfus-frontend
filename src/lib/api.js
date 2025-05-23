39675fb8a55a30877cdf2e826f382962b7f53c27
Daniel Seitenfus <dbseitenfus@inf.ufsm.br>
2025-05-23 12:49:07 -0300

    feat: create api requests and user CRUD

---
+++ src/lib/api.js
@@ -0,0 +1,39 @@
+
+const URL_BASE = "http://localhost:8000";
+
+//User
+
+export async function getUsers() {
+    const res = await fetch(`${URL_BASE}/user/`, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+    });
+    return res.json();
+}
+
+export async function createUser(user) {
+    const res = await fetch(`${URL_BASE}/user/`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(user),
+    });
+    return res.json();
+}
+
+export async function updateUser(user) {
+    const res = await fetch(`${URL_BASE}/user/`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(user),
+    });
+    return res.json();
+}
+
+export async function deleteUser(userId) {
+    const res = await fetch(`${URL_BASE}/user/${userId}/`, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data),
+    });
+    return res.json();
+}
