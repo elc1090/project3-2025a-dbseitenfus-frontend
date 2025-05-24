01fe24fd358702229931dfa3db30cc0a0170c312
Daniel Seitenfus <dbseitenfus@inf.ufsm.br>
2025-05-24 13:55:26 -0300

    refactor: changes the reading of document fields

--- src/components/document-card.jsx
+++ src/components/document-card.jsx
@@ -27,7 +27,7 @@ export default function DocumentCard({ document }) {
         <Card className="bg-stone-50" onClick={() => router.push(`/document/${document.id}`)}>
             <CardHeader className="flex flex-row items-center justify-between">
                 <FileText size={19} color="#3e9392" className="mr-3" />
-                <CardTitle className="flex-1 px-2 text-sm truncate"> {document.name}</CardTitle>
+                <CardTitle className="flex-1 px-2 text-sm truncate"> {document.title}</CardTitle>
                 <DropdownMenu>
                     <DropdownMenuTrigger onClick={(e) => e.stopPropagation()} asChild>
                         <Button variant="ghost" className="p-0 w-3 h-3">
@@ -48,10 +48,10 @@ export default function DocumentCard({ document }) {
                 </DropdownMenu>
             </CardHeader>
             <CardContent>
-                <p className="line-clamp-2 bg-white h-[100px] p-1.5 text-xs">{document.text}</p>
+                <p className="line-clamp-2 bg-white h-[100px] p-1.5 text-xs">{document.content}</p>
             </CardContent>
             <CardFooter>
-                <p className="text-xs">Criado por {document.owner.name} • {document.createdAt.toLocaleDateString()}</p>
+                <p className="text-xs">Criado por {document.user.first_name} {document.user.last_name} • {new Date(document.created_at).toLocaleDateString()}</p>
             </CardFooter>
         </Card>
     )
