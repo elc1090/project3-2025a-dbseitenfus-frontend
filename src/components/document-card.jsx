df1b98a15cdd9fa3524ea17d56592666827dac9d
Daniel Seitenfus <dbseitenfus@inf.ufsm.br>
2025-05-27 04:08:04 -0300

    feat: implement delete document feature

--- src/components/document-card.jsx
+++ src/components/document-card.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useRouter } from "next/navigation"
+import { useRouter, useParams } from "next/navigation"
 
 import {
     Card,
@@ -12,6 +12,7 @@ import {
 
 import { FileText, Link, MoreVertical } from "lucide-react"
 import { Button } from "@/components/ui/button"
+import { deleteDocument } from "@/lib/api"
 
 //Dropdown Menu
 import {
@@ -23,6 +24,27 @@ import {
 
 export default function DocumentCard({ document }) {
     const router = useRouter()
+
+    async function handleDelete(e) {
+        e.stopPropagation()  // evita que o clique propague para o card (evitando redirecionamento)
+
+        const confirmDelete = window.confirm("Tem certeza que deseja excluir este documento?")
+        if (!confirmDelete) return
+
+        try {
+            const res = await deleteDocument(document.id)
+            if (res.status === 204) {
+                alert("Documento excluído com sucesso!")
+                router.refresh() 
+            } else {
+                console.log("Erro ao excluir o documento.")
+            }
+        } catch (error) {
+            alert(error.message)
+        }
+    }
+
+
     return (
         <Card className="bg-stone-50" onClick={() => router.push(`/document/${document.id}`)}>
             <CardHeader className="flex flex-row items-center justify-between">
@@ -38,17 +60,14 @@ export default function DocumentCard({ document }) {
                         <DropdownMenuCheckboxItem>
                             Abrir em nova aba
                         </DropdownMenuCheckboxItem>
-                        <DropdownMenuCheckboxItem>
-                            Editar
-                        </DropdownMenuCheckboxItem>
-                        <DropdownMenuCheckboxItem>
+                        <DropdownMenuCheckboxItem onClick={handleDelete}>
                             Excluir
                         </DropdownMenuCheckboxItem>
                     </DropdownMenuContent>
                 </DropdownMenu>
             </CardHeader>
             <CardContent>
-                <p className="line-clamp-2 bg-white h-[100px] p-1.5 text-xs">{document.content}</p>
+                <p className="line-clamp-2 bg-white h-[100px] p-1.5 text-xs">{document.plain_text}</p>
             </CardContent>
             <CardFooter>
                 <p className="text-xs">Criado por {document.user.first_name} {document.user.last_name} • {new Date(document.created_at).toLocaleDateString()}</p>
