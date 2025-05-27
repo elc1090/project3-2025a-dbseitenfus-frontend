a4405694add149ff79e3ced97b7c56e3b72ede82
Daniel Seitenfus <dbseitenfus@inf.ufsm.br>
2025-05-27 04:05:30 -0300

    feat: create api requests

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,29 +1,68 @@
 
-const URL_BASE = "http://localhost:8000";
+const URL_BASE = "http://localhost:8000/api";
+
+function getApiToken() {
+    return `Token ${localStorage.getItem("token")}`;
+}
+
+// Authentication
+export async function login(username, password) {
+    const res = await fetch(`${URL_BASE}/login/`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password }),
+    });
+
+    if (!res.ok) {
+        throw new Error(`Login failed: ${res.statusText}`);
+    }
+
+    return res;
+}
 
 //User
+
+export async function getAuthenticatedUser() {
+    const res = await fetch(`${URL_BASE}/user/`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': getApiToken()
+        }
+    });
+    return res.json();
+}
 
 export async function getUsers() {
     const res = await fetch(`${URL_BASE}/user/`, {
         method: 'GET',
-        headers: { 'Content-Type': 'application/json' }
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': getApiToken()
+        },
     });
     return res.json();
 }
 
 export async function createUser(user) {
-    const res = await fetch(`${URL_BASE}/user/`, {
+    const res = await fetch(`${URL_BASE}/register/`, {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+        headers: {
+            'Content-Type': 'application/json'
+        },
         body: JSON.stringify(user),
     });
-    return res.json();
+    return res;
 }
 
 export async function updateUser(user) {
     const res = await fetch(`${URL_BASE}/user/`, {
         method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': getApiToken()
+        },
         body: JSON.stringify(user),
     });
     return res.json();
@@ -32,7 +71,10 @@ export async function updateUser(user) {
 export async function deleteUser(userId) {
     const res = await fetch(`${URL_BASE}/user/${userId}/`, {
         method: 'DELETE',
-        headers: { 'Content-Type': 'application/json' },
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': getApiToken()
+        },
         body: JSON.stringify(data),
     });
     return res.json();
@@ -41,19 +83,76 @@ export async function deleteUser(userId) {
 // Document
 
 export async function getDocuments() {
-    const res = await fetch(`${URL_BASE}/document/`, {
+    const res = await fetch(`${URL_BASE}/documents/`, {
         method: 'GET',
-        headers: { 'Content-Type': 'application/json' }
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': getApiToken()
+        }
     });
+
+    const data = await res.json();
+  
+  return Array.isArray(data) ? data : []; 
+}
+
+export async function createDocument(document) {
+    const res = await fetch(`${URL_BASE}/documents/`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': getApiToken()
+        },
+        body: JSON.stringify(document),
+    });
+    return res.json()
+}
+
+export async function getDocument(documentId) {
+    const res = await fetch(`${URL_BASE}/documents/${documentId}/`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': getApiToken()
+        }
+    });
+
+    if (!res.ok) {
+        console.log(`Error fetching document with ID ${documentId}: ${res.statusText}`);
+    }
 
     return res.json();
 }
 
-export async function createDocument(document) {
-    const res = await fetch(`${URL_BASE}/document/`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(document),
+export async function saveDocument(documentId, title, content, plain_text) {
+    const res = await fetch(`${URL_BASE}/documents/${documentId}/save/`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': getApiToken()
+        },
+        body: JSON.stringify({
+            title,
+            content,
+            plain_text
+        })
+    })
+
+    return res.json();
+}
+
+export async function deleteDocument(documentId) {
+    const res = await fetch(`${URL_BASE}/documents/${documentId}/`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': getApiToken()
+        }
     });
-    return res.json()
+
+    if (!res.ok) {
+        console.log(`Error deleting document with ID ${documentId}: ${res.statusText}`);
+    }
+
+    return res;
 }
